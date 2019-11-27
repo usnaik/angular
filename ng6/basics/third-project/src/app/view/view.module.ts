798cb97d7db7df37773c653e7c430007bb1a5395
usnaik <upendra_naik@yahoo.com>
2019-11-27 16:44:54 +0530

    Service and REST httpClient service with angular

---
+++ ng6/basics/third-project/src/app/view/view.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewCompComponent } from './view-comp/view-comp.component';
+
+
+
+@NgModule({
+  declarations: [ViewCompComponent],
+  imports: [CommonModule],
+  exports: [ViewCompComponent]
+})
+
+export class ViewModule { }
