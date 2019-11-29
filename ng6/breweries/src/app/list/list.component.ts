f814cf5629eb5dd7ffb68cbfab8478455a1282f7
usnaik <upendra_naik@yahoo.com>
2019-11-29 13:30:40 +0530

    Added Styping to router-intro and created a new project

---
+++ ng6/breweries/src/app/list/list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+
+export class ListComponent implements OnInit {
+
+  constructor(private _http: HttpService) { }
+
+  ngOnInit() {
+    this._http.myMethod();
+  }
+}
